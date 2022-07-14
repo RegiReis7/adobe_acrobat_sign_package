6c1fa8b5646e1a406c7bff0543abb9b6d3e2798b
RegiReis7 <reginaldo@live.com.pt>
2022-07-14 16:51:09 -0300

    Package update

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-import { AcrobatSign } from "./libs/acrobatSign";
-
-const Adobe_Sign = new AcrobatSign(
-  "3AAABLblqZhDsaXQRsxlUuk6tM3aqj6XYGbi8uGRYJUXruPeg0uPUzrtLuQbWB8lfQRkp-NPQ7LCCFN7dGB8WLgivQLQqE3VS",
-  "reginaldo.reis+demo2@vilt-group.com"
-);
-
-Adobe_Sign.agreements.get.agreementFormData("CBJCHBCAABAAYB0lkHp3bH0emH21KusKd2iDRKLg_JnG")
-  .then(async (result) => {
-    console.log(result)
-  })
-  .catch((error) => console.error(error));
+export { AcrobatSign } from "./libs/acrobatSign";
