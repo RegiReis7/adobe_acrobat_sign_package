4ceb85bf012a3615fa02352a48d50fb15c3df5af
RegiReis7 <reginaldo@live.com.pt>
2022-07-15 15:55:03 -0300

    Updated the data type from widget from data

--- src/API/widgets/GET/widgetFormData.ts
+++ src/API/widgets/GET/widgetFormData.ts
@@ -1,12 +1,19 @@
 import axios, { AxiosError } from "axios";
 import { Headers } from "../../config/types";
 
+export interface WidgetFormData {
+  formDataList: [];
+  page: {
+    nextCursor: string;
+  };
+}
+
 export async function getWidgetFormData(
   url: string,
   headers: Headers,
   widgetId: string,
   nextCursor?: string
-): Promise<{ formDataList: [] }> {
+): Promise<WidgetFormData> {
   try {
     const response = await axios.get(`${url}/widgets/${widgetId}/formData`, {
       headers,
